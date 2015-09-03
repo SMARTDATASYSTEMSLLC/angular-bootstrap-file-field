a6a26c32a6ddd0c02171348910a4f4a76fd5353b
Steve Gentile <steven.gentile@gmail.com>
2015-09-03 09:25:16 -0400

    added executable and file size

--- src/angular-bootstrap-file-field.js
+++ src/angular-bootstrap-file-field.js
@@ -7,7 +7,7 @@
  * License: MIT
  */
 
-angular.module('bootstrap.fileField',[])
+angular.module('bootstrap.fileField',['toastr'])
 .directive('fileField', function() {
   return {
     require:'ngModel',
@@ -18,20 +18,43 @@ angular.module('bootstrap.fileField',[])
             element.addClass('btn');
         }
 
-        var fileField = element.find('input');
+        var fileField = element.find('input'),
+            executables = ['.EXE', '.DMG', '.PIF','.APPLICATION', '.GADGET', '.MSI', '.MSP', '.COM', '.SCR', '.HTA', '.CPL', '.MSC', '.JAR', '.BAT', '.CMD', '.VB', '.VBE', '.JS', '.JSE', '.WS', '.WSF', '.WSC', '.WSH', '.PS1', '.PS1XML', '.PS2', '.PS2XML', '.PSC1', '.PSC2', '.MSH', '.MSH1', '.MSH2','.MSHXML', '.MSH1XML', '.MSH2XML', '.SCF', '.LNK', '.INF', '.REG', '.DOCM', '.DOTM', '.XLSM', '.XLTM', '.XLAM', '.PPTM', '.POTM', '.PPAM', '.PPSM', '.SLDM','.ACTION', '.APK', '.APP', '.COMMAND', '.CSH', '.IPA', '.PKG', '.KSH', '.OSX', '.OUT', '.PRG', '.RUN', '.WORKFLOW'],
+            enableFileSizeLimit = attrs.enableFileSizeLimit || true,
+            fileSizeLimit = attrs.maxFileSize || 5 * 1024 * 1024,
+            restrictExecutables = attrs.restrictExecutables || true;
+
+        function formatBytes(bytes) {
+            if(bytes < 1024) return bytes + " Bytes";
+            else if(bytes < 1048576) return(bytes / 1024).toFixed(0) + " KB";
+            else if(bytes < 1073741824) return(bytes / 1048576).toFixed(0) + " MB";
+            else return(bytes / 1073741824).toFixed(0) + " GB";
+        };
 
         fileField.bind('change', function(event){
             scope.$evalAsync(function () {
-              ngModel.$setViewValue(event.target.files[0]);
-              if(attrs.preview){
-                var reader = new FileReader();
-                reader.onload = function (e) {
-                    scope.$evalAsync(function(){
-                        scope[attrs.preview]=e.target.result;
-                    });
-                };
-                reader.readAsDataURL(event.target.files[0]);
-              }
+                var file = event.target.files[0],
+                    ext = file.name.substr(file.name.lastIndexOf('.')).toUpperCase();
+                if (enableFileSizeLimit && file.size > fileSizeLimit) {
+                    event.target.value = '';
+                    event.target.focus();
+                    toastr.error('File cannot be larger than ' + formatBytes(fileSizeLimit), 'Error');
+                }else if (restrictExecutables && _.indexOf(executables, ext) > -1){
+                    event.target.value = '';
+                    event.target.focus();
+                    toastr.error('File cannot be an executable', 'Error');
+                }else {
+                    ngModel.$setViewValue(file);
+                    if (attrs.preview) {
+                        var reader = new FileReader();
+                        reader.onload = function (e) {
+                            scope.$evalAsync(function () {
+                                scope[attrs.preview] = e.target.result;
+                            });
+                        };
+                        reader.readAsDataURL(file);
+                    }
+                }
             });
         });
         fileField.bind('click',function(e){
